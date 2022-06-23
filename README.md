2b64212533933fc6c7ebab1f2913ed35059ee63c
DeW-ill <a_freittaswill@outlook.com>
2022-06-23 01:43:02 -0300

    Server sttings

--- README.md
+++ README.md
@@ -9,5 +9,4 @@ Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app w
 ## How to run the project
 
 Install dependencies. To do this, type the command <strong>npm install</strong> in the project directory.
-Then go to the <i>src/assets/server</i> folder using the command prompt or gti and run the command <strong>node serve.js</strong>. With that the API will be running.
-Finally, enter the project folder again and run <strong>ng serve</strong>
+Enter the project folder and run <strong>npm run dev</strong>
--- src/app/courses/course-module.ts
+++ src/app/courses/course-module.ts
@@ -1,22 +1,16 @@
-import { NgModule } from "@angular/core";
-import { FormsModule } from "@angular/forms";
-import { RouterModule } from "@angular/router";
+import { NgModule } from '@angular/core'
+import { FormsModule } from '@angular/forms'
+import { RouterModule } from '@angular/router'
 
-import { CourseInfoComponent } from "./course-info.component";
-import { CourseListComponent } from "./course-list.component";
-import { CommonModule } from "@angular/common"
-import { StarModule } from "../shared/component/star-module";
-import { AppPipeModule } from "../shared/pipe/app-pipe.module";
-import { CoreModule } from "../core/component/core-module";
-
+import { CourseInfoComponent } from './course-info.component'
+import { CourseListComponent } from './course-list.component'
+import { CommonModule } from '@angular/common'
+import { StarModule } from '../shared/component/star-module'
+import { AppPipeModule } from '../shared/pipe/app-pipe.module'
+import { CoreModule } from '../core/component/core-module'
 
 @NgModule({
-  declarations: [
-    CourseListComponent,
-    CourseInfoComponent,
-
-
-  ],
+  declarations: [CourseListComponent, CourseInfoComponent],
   imports: [
     CommonModule,
     FormsModule,
@@ -25,16 +19,14 @@ import { CoreModule } from "../core/component/core-module";
     CoreModule,
     RouterModule.forChild([
       {
-        path: 'courses', component: CourseListComponent
+        path: 'courses',
+        component: CourseListComponent,
       },
       {
-        path: 'courses/info/:id', component: CourseInfoComponent
+        path: 'courses/info/:id',
+        component: CourseInfoComponent,
       },
-
-    ])
-  ]
+    ]),
+  ],
 })
-
-export class CourseModule {
-
-}
+export class CourseModule {}
