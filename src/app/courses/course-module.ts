33e211b6c38655906674e3f380bbcedc0348e342
DeW-ill <a_freittaswill@outlook.com>
2022-06-23 00:02:21 -0300

    Transfer profile

---
+++ src/app/courses/course-module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
+
+import { CourseInfoComponent } from "./course-info.component";
+import { CourseListComponent } from "./course-list.component";
+import { CommonModule } from "@angular/common"
+import { StarModule } from "../shared/component/star-module";
+import { AppPipeModule } from "../shared/pipe/app-pipe.module";
+import { CoreModule } from "../core/component/core-module";
+
+
+@NgModule({
+  declarations: [
+    CourseListComponent,
+    CourseInfoComponent,
+
+
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    StarModule,
+    AppPipeModule,
+    CoreModule,
+    RouterModule.forChild([
+      {
+        path: 'courses', component: CourseListComponent
+      },
+      {
+        path: 'courses/info/:id', component: CourseInfoComponent
+      },
+
+    ])
+  ]
+})
+
+export class CourseModule {
+
+}
